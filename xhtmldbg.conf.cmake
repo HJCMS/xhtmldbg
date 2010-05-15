f163f2632688cedd04c8786231c31d8f58a18d88
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2010-05-15 19:43:00 +0200

    Sa 15. Mai 22:43:49 CEST 2010

---
+++ xhtmldbg.conf.cmake
@@ -0,0 +1,22 @@
+[General]
+Version="@XHTMLDBG_VERSION@"
+DefaultFontSize=14
+DefaultFixedFontSize=14
+css_lang=en
+css_medium=all
+css_warning=2
+css_appl=@JAVA_RUNTIME@
+enableHighlightBackground=true
+enableHighlightBorder=false
+enableProxy=false
+sslCaCertsDatabase=@CA_BUNDLE_FILE@
+
+[HeaderDefinitions]
+Accept-Charset="utf-8, unicode-1-1;q=0.8"
+
+[CookieArrangement]
+hjcms.de=2
+
+[TrustedCertsHosts]
+1\host=hjcms.de
+size=1
