97abd12e9fe3fe25d5f928b0166cff97b0d8cf6e
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2010-06-05 06:18:00 +0200

    Sa 5. Jun 10:38:58 CEST 2010

---
+++ src/plugins/webkitinspector/webinspectorclient.cpp
@@ -0,0 +1,90 @@
+/**
+* This file is part of the xhtmldbg project
+*
+* Copyright (C) Juergen Heinemann http://www.hjcms.de, (C) 2007-2010
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* along with this library; see the file COPYING.LIB.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+**/
+
+#include "webinspectorclient.h"
+
+/* QtCore */
+#include <QtCore/QDebug>
+#include <QtCore/QDir>
+
+/* QtGui */
+#include <QtGui/QDesktopServices>
+
+/* QtWebKit */
+#include <QtWebKit/QWebFrame>
+#include <QtWebKit/QWebSettings>
+
+/* QtNetwork */
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkDiskCache>
+#include <QtNetwork/QNetworkAccessManager>
+
+WebInspectorClient::WebInspectorClient ( QObject * parent )
+    : QWebPage ( parent )
+{
+  setObjectName ( QLatin1String ( "webinspectorpage" ) );
+  setForwardUnsupportedContent ( false );
+  setLinkDelegationPolicy ( QWebPage::DontDelegateLinks );
+
+  // Plattenspeicher für die Performanse steigerung
+  QDir dir ( QDesktopServices::storageLocation ( QDesktopServices::CacheLocation ) );
+  dir.mkpath ( QLatin1String ( "inspector" ) );
+  storagePath = dir.path() + dir.separator() + QLatin1String ( "inspector" );
+
+  QNetworkDiskCache* diskCache = new QNetworkDiskCache ( this );
+  diskCache->setCacheDirectory ( storagePath );
+
+  networkAccessManager()->setCache ( diskCache );
+  // Einstellungen ändern
+  updateWebSettings();
+}
+
+/**
+* Das lesen aller Web Einstellungen muss
+* vor dem ersten erstellen ein tabs erfolgen.
+*/
+void WebInspectorClient::updateWebSettings()
+{
+  QWebSettings* wcfg = settings();
+  wcfg->setIconDatabasePath ( storagePath );
+  wcfg->setLocalStoragePath ( storagePath );
+  wcfg->setDefaultTextEncoding ( QLatin1String ( "utf-8" ) );
+  wcfg->setAttribute ( QWebSettings::DeveloperExtrasEnabled, true );
+  wcfg->setAttribute ( QWebSettings::OfflineStorageDatabaseEnabled, true );
+  wcfg->setAttribute ( QWebSettings::OfflineWebApplicationCacheEnabled, false );
+  wcfg->setAttribute ( QWebSettings::AutoLoadImages, true );
+  wcfg->setAttribute ( QWebSettings::JavascriptEnabled, true );
+  wcfg->setAttribute ( QWebSettings::PluginsEnabled, false );
+  wcfg->setAttribute ( QWebSettings::JavaEnabled, false );
+  wcfg->setAttribute ( QWebSettings::PrivateBrowsingEnabled, false );
+}
+
+void WebInspectorClient::load ( const QUrl &url )
+{
+  if ( url.isValid() && url.scheme().contains ( "http" ) )
+    mainFrame()->load ( url );
+}
+
+WebInspectorClient::~WebInspectorClient ()
+{
+  settings()->setAttribute ( QWebSettings::DeveloperExtrasEnabled, false );
+  networkAccessManager()->cache()->clear();
+}
