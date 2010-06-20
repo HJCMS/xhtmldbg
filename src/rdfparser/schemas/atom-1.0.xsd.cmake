ec7e27d330447941a966660c4d588389d4330c8c
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2010-06-20 07:31:00 +0200

    So 20. Jun 10:58:40 CEST 2010

---
+++ src/rdfparser/schemas/atom-1.0.xsd.cmake
@@ -0,0 +1,212 @@
+<?xml version='1.0' encoding='UTF-8' ?>
+<!--//
+* This file is part of the xhtmldbg project
+*
+* Copyright (C) Juergen Heinemann http://www.hjcms.de, (C) 2007-2010
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* along with this library; see the file COPYING.LIB.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+//-->
+<xs:schema targetNamespace="http://www.w3.org/2005/Atom"
+           elementFormDefault="qualified"
+           attributeFormDefault="unqualified"
+           xmlns:atom="http://www.w3.org/2005/Atom"
+           xmlns:xs="http://www.w3.org/2001/XMLSchema">
+
+  <xs:annotation>
+    <xs:documentation>XML Schema for Validate the ATOM 1.0 Markup Language with "xhtmldbg"</xs:documentation>
+    <xs:documentation>Maintainer: Juergen Heinemann (Undefined) http://www.hjcms.de</xs:documentation>
+    <xs:documentation>Copyright: GPLv3</xs:documentation>
+    <xs:documentation>MimeType: application/atom+xml</xs:documentation>
+  </xs:annotation>
+
+  <xs:import namespace="http://www.w3.org/XML/1998/namespace" schemaLocation="@CMAKE_INSTALL_PREFIX@/share/xhtmldbg/schemas/xml.xsd" />
+
+  <xs:element name="feed" type="atom:feedType"/>
+  <xs:element name="entry" type="atom:entryType"/>
+
+  <xs:complexType name="textType" mixed="true">
+    <xs:sequence>
+      <xs:any namespace="http://www.w3.org/1999/xhtml" minOccurs="0"/>
+    </xs:sequence>
+    <xs:attribute name="type" >
+      <xs:simpleType>
+        <xs:restriction base="xs:token">
+          <xs:enumeration value="text"/>
+          <xs:enumeration value="html"/>
+          <xs:enumeration value="xhtml"/>
+        </xs:restriction>
+      </xs:simpleType>
+    </xs:attribute>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="personType">
+    <xs:choice minOccurs="1" maxOccurs="unbounded">
+      <xs:element name="name" type="xs:string" minOccurs="1" maxOccurs="1" />
+      <xs:element name="uri" type="atom:uriType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="email" type="atom:emailType" minOccurs="0" maxOccurs="1" />
+      <xs:any namespace="##other"/>
+    </xs:choice>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:simpleType name="emailType">
+    <xs:restriction base="xs:normalizedString">
+      <xs:pattern value="\w+@(\w+\.)+\w+" />
+    </xs:restriction>
+  </xs:simpleType>
+
+  <xs:complexType name="feedType">
+    <xs:choice minOccurs="3" maxOccurs="unbounded">
+      <xs:element name="author" type="atom:personType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="category" type="atom:categoryType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="contributor" type="atom:personType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="generator" type="atom:generatorType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="icon" type="atom:iconType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="id" type="atom:idType" minOccurs="1" maxOccurs="1" />
+      <xs:element name="link" type="atom:linkType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="logo" type="atom:logoType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="rights" type="atom:textType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="subtitle" type="atom:textType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="title" type="atom:textType" minOccurs="1" maxOccurs="1" />
+      <xs:element name="updated" type="atom:dateTimeType" minOccurs="1" maxOccurs="1" />
+      <xs:element name="entry" type="atom:entryType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:any namespace="##other" minOccurs="0" maxOccurs="unbounded"/>
+    </xs:choice>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="entryType">
+    <xs:choice maxOccurs="unbounded">
+      <xs:element name="author" type="atom:personType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="category" type="atom:categoryType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="content" type="atom:contentType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="contributor" type="atom:personType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="id" type="atom:idType" minOccurs="1" maxOccurs="1" />
+      <xs:element name="link" type="atom:linkType" minOccurs="0" maxOccurs="unbounded" />
+      <xs:element name="published" type="atom:dateTimeType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="rights" type="atom:textType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="source" type="atom:textType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="summary" type="atom:textType" minOccurs="0" maxOccurs="1" />
+      <xs:element name="title" type="atom:textType" minOccurs="1" maxOccurs="1" />
+      <xs:element name="updated" type="atom:dateTimeType" minOccurs="1" maxOccurs="1" />
+      <xs:any namespace="##other" minOccurs="0" maxOccurs="unbounded"/>
+    </xs:choice>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="contentType" mixed="true">
+    <xs:sequence>
+      <xs:any namespace="##other" minOccurs="0" maxOccurs="unbounded" />
+    </xs:sequence>
+    <xs:attribute name="type" type="xs:string"/>
+    <xs:attribute name="src" type="xs:anyURI"/>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="categoryType">
+    <xs:attribute name="term" type="xs:string" use="required"/>
+    <xs:attribute name="scheme" type="xs:anyURI" use="optional"/>
+    <xs:attribute name="label" type="xs:string" use="optional"/>
+    <xs:attributeGroup ref="atom:commonAttributes" />
+  </xs:complexType>
+
+  <xs:complexType name="generatorType">
+    <xs:simpleContent>
+      <xs:extension base="xs:string">
+        <xs:attribute name="uri" use="optional" type="xs:anyURI" />
+        <xs:attribute name="version" use="optional" type="xs:string" />
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="iconType">
+    <xs:simpleContent>
+      <xs:extension base="xs:anyURI">
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="idType">
+    <xs:simpleContent>
+      <xs:extension base="xs:anyURI">
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="linkType" mixed="true">
+    <xs:attribute name="href" use="required" type="xs:anyURI" />
+    <xs:attribute name="rel" type="xs:string" use="optional"/>
+    <xs:attribute name="type" use="optional" type="xs:string" />
+    <xs:attribute name="hreflang" use="optional" type="xs:NMTOKEN" />
+    <xs:attribute name="title" use="optional" type="xs:string" />
+    <xs:attribute name="length" use="optional" type="xs:positiveInteger" />
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="logoType">
+    <xs:simpleContent>
+      <xs:extension base="xs:anyURI">
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="sourceType">
+    <xs:choice maxOccurs="unbounded">
+      <xs:element name="author" type="atom:personType" minOccurs="0" maxOccurs="unbounded"/>
+      <xs:element name="category" type="atom:categoryType" minOccurs="0" maxOccurs="unbounded"/>
+      <xs:element name="contributor" type="atom:personType" minOccurs="0" maxOccurs="unbounded"/>
+      <xs:element name="generator" type="atom:generatorType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="icon" type="atom:iconType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="id" type="atom:idType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="link" type="atom:linkType" minOccurs="0" maxOccurs="unbounded"/>
+      <xs:element name="logo" type="atom:logoType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="rights" type="atom:textType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="subtitle" type="atom:textType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="title" type="atom:textType" minOccurs="0" maxOccurs="1"/>
+      <xs:element name="updated" type="atom:dateTimeType" minOccurs="0" maxOccurs="1"/>
+      <xs:any namespace="##other" minOccurs="0" maxOccurs="unbounded"/>
+    </xs:choice>
+    <xs:attributeGroup ref="atom:commonAttributes"/>
+  </xs:complexType>
+
+  <xs:complexType name="uriType">
+    <xs:simpleContent>
+      <xs:extension base="xs:anyURI">
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="dateTimeType">
+    <xs:simpleContent>
+      <xs:extension base="xs:dateTime">
+        <xs:attributeGroup ref="atom:commonAttributes"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:attributeGroup name="commonAttributes">
+    <xs:attribute ref="xml:base" />
+    <xs:attribute ref="xml:lang" />
+    <xs:anyAttribute namespace="##other"/>
+  </xs:attributeGroup>
+
+</xs:schema>
---
+++ src/rdfparser/schemas/rss-2.0.xsd.cmake
@@ -0,0 +1,229 @@
+<?xml version='1.0' encoding='UTF-8' ?>
+<!--//
+* This file is part of the xhtmldbg project
+*
+* Copyright (C) Juergen Heinemann http://www.hjcms.de, (C) 2007-2010
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* along with this library; see the file COPYING.LIB.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+//-->
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+           xmlns:atom="http://purl.org/atom/ns#"
+           elementFormDefault="unqualified"
+           attributeFormDefault="unqualified"
+           xml:lang="en" version="0.8.8">
+
+  <xs:annotation>
+    <xs:documentation>XML Schema for Validate the RSS 2.0 Markup Language with "xhtmldbg"</xs:documentation>
+    <xs:documentation>Maintainer: Juergen Heinemann (Undefined) http://www.hjcms.de</xs:documentation>
+    <xs:documentation>Copyright: GPLv3</xs:documentation>
+    <xs:documentation>MimeType: application/rss+xml</xs:documentation>
+  </xs:annotation>
+
+  <xs:import namespace="http://www.w3.org/XML/1998/namespace" schemaLocation="@CMAKE_INSTALL_PREFIX@/share/xhtmldbg/schemas/xml.xsd" />
+
+  <!-- rss -->
+  <xs:element name="rss">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element name="channel" type="channelNode" minOccurs="1" maxOccurs="1" />
+        <xs:any namespace="http://www.w3.org/2005/Atom" processContents="lax"
+                minOccurs="0" maxOccurs="unbounded" />
+      </xs:sequence>
+      <xs:attribute name="version" type="xs:decimal" use="required" fixed="2.0" />
+      <xs:anyAttribute namespace="##other"/>
+    </xs:complexType>
+  </xs:element>
+
+  <!-- channel -->
+  <xs:complexType name="channelNode">
+    <xs:sequence>
+      <xs:choice maxOccurs="unbounded">
+        <!-- Required -->
+        <xs:element name="title" type="xs:string" minOccurs="1" />
+        <xs:element name="link" type="xs:anyURI" minOccurs="1" />
+        <xs:element name="description" type="xs:string" minOccurs="1" />
+        <!-- Optional -->
+        <xs:element name="language" type="xs:language" minOccurs="0" />
+        <xs:element name="copyright" type="xs:string" minOccurs="0" />
+        <xs:element name="managingEditor" type="xs:string" minOccurs="0" />
+        <xs:element name="webMaster" type="personResponsibleNode" minOccurs="0" />
+        <xs:element name="pubDate" type="dateNode" minOccurs="0" />
+        <xs:element name="lastBuildDate" type="dateNode" minOccurs="0" />
+        <xs:element name="category" type="categoryNode" minOccurs="0" />
+        <xs:element name="generator" type="xs:string" minOccurs="0" />
+        <xs:element name="docs" type="xs:string" minOccurs="0" />
+        <xs:element name="cloud" type="cloudNode" minOccurs="0" />
+        <xs:element name="ttl" type="xs:positiveInteger" minOccurs="0" />
+        <xs:element name="image" type="imageNode" minOccurs="0" />
+        <xs:element name="rating" type="xs:string" minOccurs="0" />
+        <xs:element name="textInput" type="textInputNode" minOccurs="0" />
+        <xs:element name="skipHours" type="skipHoursNode" minOccurs="0" />
+        <xs:element name="skipDays" type="skipDaysNode" minOccurs="0" />
+        <xs:element name="item" type="itemNodes" minOccurs="1" />
+      </xs:choice>
+    </xs:sequence>
+  </xs:complexType>
+
+  <!-- item -->
+  <xs:complexType name="itemNodes">
+    <xs:choice maxOccurs="unbounded">
+      <!-- Required -->
+      <xs:element name="title" type="xs:string" minOccurs="1" />
+      <xs:element name="link" type="xs:anyURI" minOccurs="1" />
+      <xs:element name="description" type="descriptionNode" minOccurs="1" />
+      <!-- Optional -->
+      <xs:element name="author" type="personResponsibleNode" minOccurs="0" />
+      <xs:element name="category" type="categoryNode" minOccurs="0" />
+      <xs:element name="comments" type="xs:string" minOccurs="0" />
+      <xs:element name="enclosure" type="enclosureNode" minOccurs="0" />
+      <xs:element name="guid" type="guidNode" minOccurs="0" />
+      <xs:element name="pubDate" type="dateNode" minOccurs="0" />
+      <xs:element name="source" type="sourceNode" minOccurs="0" />
+    </xs:choice>
+  </xs:complexType>
+
+  <!-- Definitions -->
+  <xs:simpleType name="dateNode">
+    <xs:restriction base="xs:string">
+      <xs:pattern value="(((Mon)|(Tue)|(Wed)|(Thu)|(Fri)|(Sat)|(Sun)), *)?\d\d? +((Jan)|(Feb)|(Mar)|(Apr)|(May)|(Jun)|(Jul)|(Aug)|(Sep)|(Oct)|(Nov)|(Dec)) +\d\d(\d\d)? +\d\d:\d\d(:\d\d)? +(([+\-]?\d\d\d\d)|(UT)|(GMT)|(EST)|(EDT)|(CST)|(CDT)|(MST)|(MDT)|(PST)|(PDT)|\w)" />
+    </xs:restriction>
+  </xs:simpleType>
+
+  <xs:simpleType name="personResponsibleNode">
+    <xs:restriction base="xs:string">
+      <xs:pattern value="([a-zA-Z0-9_\-])([a-zA-Z0-9_\-\.]*)@(\[((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}|((([a-zA-Z0-9\-]+)\.)+))([a-zA-Z]{2,}|(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\])"/>
+    </xs:restriction>
+  </xs:simpleType>
+
+  <xs:complexType name="sourceNode">
+    <xs:simpleContent>
+      <xs:extension base="xs:string">
+        <xs:attribute name="url" type="xs:anyURI"/>
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="enclosureNode">
+    <xs:simpleContent>
+      <xs:extension base="xs:string">
+        <xs:attribute name="url" type="xs:anyURI" use="required" />
+        <xs:attribute name="length" type="xs:byte" use="required" />
+        <xs:attribute name="type" type="xs:string" use="required" />
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="categoryNode">
+    <xs:simpleContent>
+      <xs:extension base="xs:string">
+        <xs:attribute name="domain" type="xs:anyURI" use="optional" />
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <xs:complexType name="cloudNode" block="restriction">
+    <xs:attribute name="domain" type="xs:anyURI" use="required" />
+    <xs:attribute name="port" type="xs:positiveInteger" use="required" />
+    <xs:attribute name="path" type="xs:anyURI" use="required" />
+    <xs:attribute name="registerProcedure" type="xs:anyURI" use="required" />
+    <xs:attribute name="protocol" type="cloudAttributeProtocol" use="required" />
+  </xs:complexType>
+
+  <xs:complexType name="textInputNode" block="restriction">
+    <xs:all>
+      <xs:element name="title" type="xs:string" minOccurs="1" />
+      <xs:element name="description" type="descriptionNode" minOccurs="1" />
+      <xs:element name="link" type="xs:anyURI" minOccurs="1" />
+      <xs:element name="name" type="xs:string" minOccurs="1" />
+    </xs:all>
+  </xs:complexType>
+
+  <xs:complexType name="skipHoursNode" block="restriction">
+    <xs:sequence>
+      <xs:element name="hour" minOccurs="0" maxOccurs="24">
+        <xs:simpleType>
+          <xs:restriction base="xs:nonNegativeInteger">
+            <xs:minInclusive value="0"/>
+            <xs:maxInclusive value="23"/>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:element>
+    </xs:sequence>
+  </xs:complexType>
+
+  <xs:complexType name="skipDaysNode" block="restriction">
+    <xs:sequence>
+      <xs:element name="day" minOccurs="1" maxOccurs="7">
+        <xs:simpleType>
+          <xs:restriction base="xs:string">
+            <xs:enumeration value="Monday"/>
+            <xs:enumeration value="Tuesday"/>
+            <xs:enumeration value="Wednesday"/>
+            <xs:enumeration value="Thursday"/>
+            <xs:enumeration value="Friday"/>
+            <xs:enumeration value="Saturday"/>
+            <xs:enumeration value="Sunday"/>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:element>
+    </xs:sequence>
+  </xs:complexType>
+
+  <xs:complexType name="imageNode" block="restriction">
+    <xs:all>
+      <xs:element name="url" minOccurs="1">
+        <xs:simpleType>
+          <xs:restriction base="xs:string">
+            <xs:pattern value="(http:.+\.(gif|GIF|jp[e]?g|JP[E]?G|png|PNG))"/>
+          </xs:restriction>
+        </xs:simpleType>
+      </xs:element>
+      <xs:element name="title" type="xs:string" minOccurs="1" />
+      <xs:element name="link" type="xs:anyURI" minOccurs="1" />
+    </xs:all>
+  </xs:complexType>
+
+  <xs:complexType name="descriptionNode">
+     <xs:complexContent mixed="true">
+      <xs:restriction base="xs:anyType">
+        <xs:sequence>
+          <xs:any namespace="http://www.w3.org/1999/xhtml"
+                  minOccurs="0" maxOccurs="unbounded"
+                  processContents="skip" />
+        </xs:sequence>
+        <xs:anyAttribute namespace="http://www.w3.org/1999/xhtml" />
+      </xs:restriction>
+    </xs:complexContent>
+  </xs:complexType>
+
+  <xs:complexType name="guidNode">
+    <xs:simpleContent>
+      <xs:extension base="xs:anyURI">
+        <xs:attribute name="isPermaLink" type="xs:boolean" use="optional" />
+      </xs:extension>
+    </xs:simpleContent>
+  </xs:complexType>
+
+  <!-- Attribute Definitions -->
+  <xs:simpleType name="cloudAttributeProtocol">
+    <xs:restriction base="xs:string">
+      <xs:enumeration value="xml-rpc"/>
+      <xs:enumeration value="http-post"/>
+      <xs:enumeration value="soap"/>
+    </xs:restriction>
+  </xs:simpleType>
+
+</xs:schema>
