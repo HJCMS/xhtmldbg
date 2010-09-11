520744862af1e900ce5de00cf06d602f1b361c4c
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2010-09-11 06:20:00 +0200

    Sa 11. Sep 09:50:41 CEST 2010

---
+++ build-native.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+test -f /usr/share/cdbs/1/class/cmake.mk || {
+  echo "cdbs (>= 0.4.8) not Installed."
+  exit 1
+}
+
+test -x "$(which git)" || {
+  echo "git not Installed - can not read configuration"
+  exit 1
+}
+
+test -f $PWD/.git/config || {
+  echo "this is not a git repository"
+  exit 1
+}
+
+VERSION="`git config --get package.version`"
+PACKAGER="`git config --get user.name`"
+EMAIL="`git config git config --get user.email`"
+SIGN_KEY="`git config --get user.signingkey`"
+
+export DEBSIGN_KEYID="$SIGN_KEY"
+export DEBUILD_LINTIAN=yes
+export DEBUILD_LINTIAN_OPTS="-i -I.git --show-overrides"
+
+if test -n "$SIGN_KEY" ; then
+  dpkg-buildpackage -rfakeroot -k$SIGN_KEY \
+    -v${VERSION} -F -sn -tc -Rdebian/rules
+else
+  dpkg-buildpackage -rfakeroot -v${VERSION} \
+     -F -sn -tc -Rdebian/rules
+fi
