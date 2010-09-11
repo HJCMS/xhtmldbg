45e3ea21d143b90262ee5452333c9a27d7921a99
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2010-09-11 06:20:00 +0200

    Sa 11. Sep 10:38:51 CEST 2010

--- utils/package.sh
+++ utils/package.sh
@@ -19,26 +19,28 @@ rm -rf /tmp/xhtmldbg-${version}*
 git clone ~/hjcms/xhtmldbg /tmp/xhtmldbg-${version}
 pushd /tmp/xhtmldbg-${version}
   touch .git/config
+  git prune master
   git config --file .git/config --remove-section core
   git config --file .git/config --remove-section remote.origin
-  git config --file .git/config --unset branch.master.remote
+  git config --file .git/config --remove-section remote.webmast
+  git config --file .git/config branch.master.remote "$PWD"
   git config --file .git/config --add packager.version "$ORIG_VERSION"
   git config --file .git/config --add packager.maintainer "$ORIG_PACKAGER"
   git config --file .git/config --add packager.email "$ORIG_EMAIL"
   git config --file .git/config --add packager.signingkey "$ORIG_SIGN_KEY"
 popd
 
-# pushd /tmp/
-# > md5sums.txt
-# rm -f xhtmldbg-${version}.tar.{bz2,lzma}
-# echo -e '\E[1;37;44m' " Create Archive xhtmldbg-${version}.tar.bz2" '\033[0m'
-# tar -cjf xhtmldbg-${version}.tar.bz2 xhtmldbg-${version}
-# md5sum xhtmldbg-${version}.tar.bz2 > md5sums.txt
-# 
-# echo -e '\E[1;37;44m' " Create Archive xhtmldbg-${version}.tar.lzma" '\033[0m'
-# tar -cJf xhtmldbg-${version}.tar.lzma xhtmldbg-${version}
-# md5sum xhtmldbg-${version}.tar.lzma >> md5sums.txt
-# popd
-# 
-# echo "ready"
-# exit 0
+pushd /tmp/
+> md5sums.txt
+rm -f xhtmldbg-${version}.tar.{bz2,lzma}
+echo -e '\E[1;37;44m' " Create Archive xhtmldbg-${version}.tar.bz2" '\033[0m'
+tar -cjf xhtmldbg-${version}.tar.bz2 xhtmldbg-${version}
+md5sum xhtmldbg-${version}.tar.bz2 > md5sums.txt
+
+echo -e '\E[1;37;44m' " Create Archive xhtmldbg-${version}.tar.lzma" '\033[0m'
+tar -cJf xhtmldbg-${version}.tar.lzma xhtmldbg-${version}
+md5sum xhtmldbg-${version}.tar.lzma >> md5sums.txt
+popd
+
+echo "ready"
+exit 0
