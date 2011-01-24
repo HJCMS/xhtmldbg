88f1e6ccc5f0ba47cec428d8944fd3fe83cf2a52
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2011-01-24 07:51:00 +0100

    Mo 24. Jan 13:22:06 CET 2011

--- src/cookiesdatabase/cookieshandle.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "cookieshandle" (
-  "Hostname" TEXT  UNIQUE PRIMARY KEY ASC,
-  "AccessType" INTEGER default 0,
-  "AllowThirdParty" INTEGER default 0,
-  "RFC2109" INTEGER default 1
-);
--- src/cookiesdatabase/cookiesstorage.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "cookiesstorage" (
-  "domain" TEXT,
-  "path" TEXT default '/',
-  "name" TEXT,
-  "expiration" INTEGER,
-  "data" TEXT
-);
