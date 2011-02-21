cfddfe217d80e998dc4c1085d650ba468ee4833e
Heinemann Jürgen (Undefined) <nospam __AT. hjcms __DOT. de>
2011-02-21 07:09:00 +0100

    Mo 21. Feb 12:49:46 CET 2011

--- extract-messages.sh
+++ extract-messages.sh
@@ -16,7 +16,7 @@ BASEDIR="src"
 PROJECT="xhtmldbg"
 BUGADDR="`git config author.email`"
 XGETTEXT_FLAGS_KDE="-ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3"
-XGETTEXT_FLAGS_QT="-ktr:1,1t -ktr:1,2c,2t -ktrUtf8:1,1t -kQT_TRANSLATE_NOOP:1c,2,2t -kQT_TR_NOOP:1,1t -ktranslate:1c,2,2t -ktranslate:2,3c,3t"
+XGETTEXT_FLAGS_QT="-ctrUtf8 -ktrUtf8:1"
 
 > ${TEMPFILE}
 
@@ -31,7 +31,7 @@ pushd ${base}
 
   echo "Extracting messages"
   ${XGETTEXT} --from-code=UTF-8 -C -kde ${XGETTEXT_FLAGS_KDE} ${XGETTEXT_FLAGS_QT} \
-    --msgid-bugs-address="${BUGADDR}" --copyright-holder="LGPLv3" --default-domain=${PROJECT} \
+    --msgid-bugs-address="${BUGADDR}" --copyright-holder="LGPLv3" \
     --files-from=${TEMPFILE} -D ${BASEDIR} -D $PWD -o po/${PROJECT}.pot || {
       echo "error while calling xgettext. aborting.";
       exit 1;
