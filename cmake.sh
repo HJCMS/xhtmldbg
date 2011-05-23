e8bcf66522c0cd90fdf7c30c796b6381113257b0
Heinemann Jürgen (Undefined) <heinemann.juergen@hjcms.de>
2011-05-23 10:54:00 +0200

    Mo 23. Mai 15:21:07 CEST 2011

--- cmake.sh
+++ cmake.sh
@@ -5,18 +5,18 @@
 # Copyright: See COPYING file that comes with this distribution
 #################################################################
 
-if test -f $HOME/hjcms/projectconfigurations.sh ; then
-source $HOME/hjcms/projectconfigurations.sh
+if test -f /srv/projects/projectconfigurations.sh ; then
+source /srv/projects/projectconfigurations.sh
 fi
 
 cmake -Wdev \
-	-DCMAKE_CXX_FLAGS:STRING="${CFLAGS:-"-O2"}" \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_BUILD_TYPE:STRING=Debug \
-	-DXHTMLDBG_BUILD_VERBOSE:BOOL=ON \
-	-DCMAKE_SKIP_RPATH:BOOL=ON \
-	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-	-DXHTMLDBG_EXPERIMENTAL:BOOL=ON \
-	-DPHONON_STL_INCLUDE_DIR:PATH=/usr/include/kde4/KDE \
-	-DAUTOMOC4_EXECUTABLE:FILEPATH=$(which automoc4) \
-	$CMAKE_EXTRAS $@ ../
+  -DCMAKE_CXX_FLAGS:STRING="${CFLAGS:-"-O2"}" \
+  -DCMAKE_INSTALL_PREFIX:PATH=/usr \
+  -DCMAKE_BUILD_TYPE:STRING=Debug \
+  -DXHTMLDBG_BUILD_VERBOSE:BOOL=ON \
+  -DCMAKE_SKIP_RPATH:BOOL=ON \
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+  -DXHTMLDBG_EXPERIMENTAL:BOOL=ON \
+  -DPHONON_STL_INCLUDE_DIR:PATH=/usr/include/kde4/KDE \
+  -DAUTOMOC4_EXECUTABLE:FILEPATH=$(which automoc4) \
+  $CMAKE_EXTRAS $@ ../
